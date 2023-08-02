408774c01ee6f2792f2925410bea6b20f0229261
Josue Recoba Huerta <josue.huer.reco.22@gmail.com>
2023-08-01 21:25:08 -0600

    Middleware de verificacion

--- config/config.js
+++ config/config.js
@@ -1,10 +1,15 @@
-require('dotenv').config();
-
+require("dotenv").config();
 const config = {
-  env: process.env.NODE_ENV || 'dev',
-  isProd: process.env.NODE_ENV === 'production',
-  port: process.env.PORT || 3000,
-  dbUrl: process.env.DATABASE_URL,
+    env: process.env.NODE_ENV || 'dev',
+    port: process.env.PORT || 3000,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+    isProd: process.env.NODE_ENV === 'production',
+    dbUrl: process.env.DATABASE_URL,
+    apiKey: process.env.API_KEY,
 }
 
-module.exports = { config };
+module.exports = {config}
--- index.js
+++ index.js
@@ -1,42 +1,82 @@
 const express = require('express');
 const cors = require('cors');
-const routerApi = require('./routes');
-
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const routerAPI = require('./routes/indexRoutes');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandle');
+const {checkApiKey} = require('./middlewares/authHandler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whilelist = ['http://localhost:3000'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
+    if (whilelist.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('no permitido'));
+      callback(new Error('Not access'));
     }
-  }
+  },
 }
-app.use(cors(options));
-
-app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
-});
-
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
-});
-
-routerApi(app);
-
-app.use(logErrors);
-app.use(ormErrorHandler);
-app.use(boomErrorHandler);
-app.use(errorHandler);
-
-
+app.use(cors()); //cualquier origen
+app.get('/api', (req, res) => {
+  res.send(`
+    <style>
+      html {
+        margin: 0 50px;
+        font-family: sans-serif;
+        background-color: #1a1a1a;
+      }
+      h1, p, b, li, pre {
+        color: white;
+      }
+    </style>
+    <h1>API en Express</h1>
+    <p>Urls</p>
+    <ul>
+      <b>GET</b>
+      <li>/api/v1/products</li>
+      <br><br>
+      <b>GET/POST/PATCH/DELETE</b>
+      <li>/api/v1/products/{id}</li>
+    </ul>
+    <br><br>
+    <p>Body</p>
+    <pre>
+      {
+        "name": "string",
+        "price": "number",
+        "image": "string"
+      }
+    </pre>
+  `);
+})
+routerAPI(app);
+app.use(logErrors)
+app.use(ormErrorHandler)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+app.get('/users', (req, res) => {
+  const {limit, offset} = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.json({
+      limit: 10,
+      offset: 0
+    })
+  }
+})
+app.get('/categories/:cid/products/:pid', (req, res) => {
+  const {cid, pid} = req.params;
+  res.json({
+    cid ,
+    pid,
+  })
+})
 app.listen(port, () => {
-  console.log(`Mi port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
